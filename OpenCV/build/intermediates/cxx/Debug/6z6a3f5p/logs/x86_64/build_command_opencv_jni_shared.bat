f820a96a4306586a641c79f0db14d8f7266b1c5e
Seppo Kolehmainen <seppo.kolehmainen@tuni.fi>
2022-10-20 08:46:31 +0300

    remove build dir from version control

--- OpenCV/build/intermediates/cxx/Debug/6z6a3f5p/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\kivin\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\kivin\\AndroidStudioProjects\\SeeTrafficSignsOpenCV\\OpenCV\\.cxx\\Debug\\6z6a3f5p\\x86_64" ^
-  opencv_jni_shared
